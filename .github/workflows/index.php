4161054fb8dfc045ef285a45d81a7b6b38686aec
programeelco <91141039+programeelco@users.noreply.github.com>
2021-10-19 20:40:49 +0200

    index.php
    test

--- .github/workflows/index.php
+++ .github/workflows/index.php
@@ -1,5 +1,9 @@
+<form method=get action="<?php $_SERVER["PHP_SELF"] ?>">
+<input name="site" type="url" required>
+  <input type="submit">
+</form>
 <?php
-
-echo "test";
-
+$server = $_SERVER["PHP_SELF"];
+$site = $_GET["site"];
+$open = $site ? $site : "https://www.eelcogreidanus.nl";
 ?>
